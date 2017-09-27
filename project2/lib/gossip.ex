5f8b1a0edb732db917ee9970766c62990175ba4d
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-09-27 01:38:01 -0400

    initial working code

--- project2/lib/gossip.ex
+++ project2/lib/gossip.ex
@@ -2,29 +2,34 @@ defmodule Gossip do
     def start(parent) do
         neighbours = []
         rumor_count = 0
-        listen(neighbours, rumor_count)
+        listen(neighbours, rumor_count, parent)
     end
     defp listen(neighbours, rumor_count, parent) do
         receive do
-            {:neighbours, neighbour_list} -> neighbours = neighbour_list
+            {:neighbours, neighbour_list} -> neighbours = set_neighbours(neighbour_list)
+                IO.inspect neighbours, label: "Registered neighbours"
             {:rumor, from, message} -> rumor_count = handle_rumors(message, rumor_count, neighbours, from, parent)
             {:initiate, value} -> send_rumor("secret message", neighbours)
         end
-        listen(neighbours, rumor_count)
+        listen(neighbours, rumor_count, parent)
+    end
+    defp set_neighbours(neighbours) do
+        List.delete(neighbours, self())
     end
     defp handle_rumors(message, count, neighbours, from, parent, terminate_count \\ 3) do
         IO.inspect from, label: "Received rumor from"
+        count = count + 1
         if count >=  terminate_count do
             terminate(parent)
         else
             send_rumor(message, neighbours)
-            count = count + 1
         end
         count
     end
     defp send_rumor(message, neighbours) do
         recipients = get_random_neighbours(neighbours)
         for recipient <- recipients do
+            IO.inspect recipient, label: "sending rumor to"
             send recipient, {:rumor, self(), message}
         end
     end
@@ -33,6 +38,5 @@ defmodule Gossip do
     end
     defp terminate(parent) do
         send parent, {:terminating, self(), :normal}
-        exit(self(), :normal)
     end
 end
