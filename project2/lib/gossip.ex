bb77d5d1297bf52b78f715a65ecb3ff9d67d2e22
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-09-30 15:10:01 -0400

    not killing the process after it has received a message n times

--- project2/lib/gossip.ex
+++ project2/lib/gossip.ex
@@ -4,60 +4,62 @@ defmodule Gossip do
         rumor_count = 0
         listen(neighbours, rumor_count, parent)
     end
-    defp listen(neighbours, rumor_count, parent) do
-        IO.puts "start listening: #{inspect(self())}"
+    defp listen(neighbours, rumor_count, parent, terminated \\ false) do
+        #IO.puts "start listening: #{inspect(self())}"
         receive do
             {:neighbours, neighbour_list} -> neighbours = set_neighbours(neighbour_list)
                 IO.inspect neighbours, label: "Registered neighbours"
-            {:rumor, from, message} -> {rumor_count, neighbours} = handle_rumors(message, rumor_count, neighbours, from, parent)
+            {:rumor, from, message} -> {rumor_count, neighbours, terminated} = handle_rumors(message, rumor_count, neighbours, from, parent, terminated)
             {:initiate, value} -> send_rumor("secret message", neighbours)
         end
-        listen(neighbours, rumor_count, parent)
+        listen(neighbours, rumor_count, parent, terminated)
     end
     defp set_neighbours(neighbours) do
         List.delete(neighbours, self())
     end
-    defp handle_rumors(message, count, neighbours, from, parent, terminate_count \\ 10) do
-        IO.puts "Received rumor from: #{inspect(from)} to: #{inspect(self())}"
+    defp handle_rumors(message, count, neighbours, from, parent, terminated \\ false, terminate_count \\ 10) do
+        #IO.puts "Received rumor from: #{inspect(from)} to: #{inspect(self())}"
         count = count + 1
-        if count >=  terminate_count do
+        if count >=  terminate_count or terminated do
             # Last message
             send_rumor(message, neighbours)
             terminate(parent)
+            terminated = true
         else
-            neighbours = send_rumor(message, neighbours)
-            if neighbours == [] do
-                terminate(parent)
-            end
+            send_rumor(message, neighbours)
+            # if neighbours == [] do
+            #     terminate(parent)
+            # end
         end
-        {count, neighbours}
+        {count, neighbours, terminated}
     end
     defp send_rumor(message, neighbours) do
-        #recipients = get_random_neighbours(neighbours)
+        # Testing to not kill nodes after n
+        recipients = get_random_neighbours(neighbours)
         #TODO:- active recipient always goes through all the neigbours to select an active one
-        IO.puts "Looking for recipients"
-        {recipients, neighbours} = get_active_neighbours(neighbours, MapSet.new, 0)
+        #IO.puts "Looking for recipients"
+        #{recipients, neighbours} = get_active_neighbours(neighbours, MapSet.new, 0)
         for recipient <- recipients do
-            IO.puts "sending rumor to: #{inspect(recipient)} from: #{inspect(self)}"
+            #IO.puts "sending rumor to: #{inspect(recipient)} from: #{inspect(self)}"
             send recipient, {:rumor, self(), message}
         end
         neighbours
     end
-    # defp get_random_neighbours(neighbours, number \\ 1) do
-    #     recipients = Enum.take_random(neighbours, number)
-    #     for recipient <- recipients do
-    #         if not(Process.alive?(recipient)) do
-    #             List.delete(recipients, recipients)
-    #         end
-    #     end
-    #     recipients
-    # end
-    defp get_active_neighbours(neighbours, act_recipients, size) when size == 1 do
-        IO.puts "Recipients selected: #{inspect(act_recipients)}"
+    defp get_random_neighbours(neighbours, number \\ 10) do
+        recipients = Enum.take_random(neighbours, number)
+        # for recipient <- recipients do
+        #     if not(Process.alive?(recipient)) do
+        #         List.delete(recipients, recipients)
+        #     end
+        # end
+        recipients
+    end
+    defp get_active_neighbours(neighbours, act_recipients, size) when size == 10 do
+        #IO.puts "Recipients selected: #{inspect(act_recipients)}"
         {act_recipients, neighbours}
     end
     defp get_active_neighbours(neighbours, act_recipients, size) when neighbours == [] do
-        IO.puts "neighbours exhausted"
+        #IO.puts "neighbours exhausted"
         {[], []}
     end
     defp get_active_neighbours(neighbours, act_recipients, size) do
@@ -72,6 +74,7 @@ defmodule Gossip do
     end
     defp terminate(parent) do
         send parent, {:terminating, self(), :normal}
-        Process.exit(self(), :normal)
+        #IO.puts "Killing self: #{inspect(self())}"
+        #Process.exit(self(), :normal)
     end
 end
