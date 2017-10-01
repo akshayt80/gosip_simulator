9e33289997e506d9bb8a3c6e9ba4b14ab234da5e
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-10-01 16:37:46 -0400

    calling Line topology

--- project2/lib/project2.ex
+++ project2/lib/project2.ex
@@ -16,6 +16,7 @@ defmodule Project2 do
       {_, [str], _} = OptionParser.parse(args)
       IO.puts "Building mesh topology"
       nodes = elem(Integer.parse(str), 0)
-      Mesh.build(nodes)
+      #Mesh.build(nodes)
+      Line.build(nodes)
   end
 end
