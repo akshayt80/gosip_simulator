af723fbda8ee26d9c6c912dcd64e590faf10f2a5
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-10-02 00:25:44 -0400

    commented old output line

--- project2/lib/project2.ex
+++ project2/lib/project2.ex
@@ -14,7 +14,7 @@ defmodule Project2 do
   """
   def main(args) do
       {_, [str], _} = OptionParser.parse(args)
-      IO.puts "Building mesh topology"
+      #IO.puts "Building mesh topology"
       nodes = elem(Integer.parse(str), 0)
       #Mesh.build(nodes)
       Line.build(nodes)
