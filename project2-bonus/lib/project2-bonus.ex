48f01f1ce4b1fb7d3480d14e19ca9fc0e10aed71
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2017-10-07 23:29:51 -0400

    accepting command line arguement for percentage of node affected and type of fault (node or connection)

--- project2-bonus/lib/project2-bonus.ex
+++ project2-bonus/lib/project2-bonus.ex
@@ -13,16 +13,17 @@ defmodule Project2Bonus do
 
   """
   def main(args) do
-      {_, [nodes, topology, algo], _} = OptionParser.parse(args)
+      {_, [nodes, topology, algo, node_percent, failure_type], _} = OptionParser.parse(args)
       #IO.puts "Building mesh topology"
       IO.puts "command line arguments: #{inspect(nodes)}"
       nodes = elem(Integer.parse(nodes), 0)
+      node_percent = elem(Integer.parse(node_percent), 0)
       IO.puts "algo selected: #{algo} topology: #{topology}"
       case topology do
-          "full" -> Mesh.build(nodes, :"#{algo}")
-          "line" -> Line.build(nodes, :"#{algo}")
-          "imp2d" -> Imp2d.build(nodes, :"#{algo}")
-          "2d" -> P2D.build(nodes, :"#{algo}")
+          "full" -> Mesh.build(nodes, :"#{algo}", node_percent, :"#{failure_type}")
+          "line" -> Line.build(nodes, :"#{algo}", node_percent, :"#{failure_type}")
+          "imp2d" -> Imp2d.build(nodes, :"#{algo}", node_percent, :"#{failure_type}")
+          "2d" -> P2D.build(nodes, :"#{algo}", node_percent, :"#{failure_type}")
       end
       #
       #Line.build(nodes)
